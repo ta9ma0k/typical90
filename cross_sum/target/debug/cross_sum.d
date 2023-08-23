0b91e4a8de81777ce5681bfd411fb70d1e87faac
okada takuma <takuma@pop-os.localdomain>
2023-08-23 10:22:08 +0900

    init project

---
+++ cross_sum/target/debug/cross_sum.d
@@ -0,0 +1,1 @@
+/home/takuma/Workspace/typical90/cross_sum/target/debug/cross_sum: /home/takuma/Workspace/typical90/cross_sum/src/main.rs
---
+++ cross_sum/target/debug/deps/cross_sum-fa0855454dc9f2fe.d
@@ -0,0 +1,5 @@
+/home/takuma/Workspace/typical90/cross_sum/target/debug/deps/cross_sum-fa0855454dc9f2fe: src/main.rs
+
+/home/takuma/Workspace/typical90/cross_sum/target/debug/deps/cross_sum-fa0855454dc9f2fe.d: src/main.rs
+
+src/main.rs:
