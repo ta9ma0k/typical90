18c88760842e7d3f98aeeedb82c5f3404f4c1b25
okada takuma <t-okada@NPC-18-009.local>
2023-08-28 11:10:23 +0900

    013 AC

--- we_used_to_sing_a_song_together/src/main.rs
+++ we_used_to_sing_a_song_together/src/main.rs
@@ -1,15 +1,17 @@
 fn main() {
     proconio::input! {
         n: usize,
-        mut a: [i32; n],
-        mut b: [i32; n],
+        mut a: [isize; n],
+        mut b: [isize; n],
     }
 
     a.sort();
     b.sort();
 
-    let result = vec![0..n].iter()
-        .fold(0, |acc, i| acc + (a[i] - b[i]).abs());
+    let mut result = 0;
+    for i in 0..n {
+        result += (a[i] - b[i]).abs();
+    }
 
     println!("{}", result);
 }
